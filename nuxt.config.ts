a942ad0060a50ad118b56e0aa7dcd80f90a7085c
nicola-spadari <nicola.spadari@smartpricing.it>
2025-09-19 16:13:23 +0200

    fix: nuxt ui doesn't need exports

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -49,7 +49,7 @@ export default defineNuxtConfig({
 		'#layers/smartness-nuxt-ui': join(currentDir, "./runtime/index.ts")
 	},
 	build: {
-		transpile: ["zod", "@vueuse/core", "@nuxt/ui"]
+		transpile: ["zod", "@vueuse/core"]
 	},
 	eslint: {
 		config: {
--- runtime/index.ts
+++ runtime/index.ts
@@ -1,3 +1,2 @@
 export { z } from "zod";
-export * from "@nuxt/ui";
 export type * from "@vueuse/core";
