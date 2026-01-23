8aa7fc7257ffef5205426a1d30b4b94118d927fe
Andrea Minato <andreaminato@outlook.com>
2026-01-23 10:39:16 +0100

    feat: enhance AppPage component with additional navbar slot support for improved customization

--- app/components/AppPage.vue
+++ app/components/AppPage.vue
@@ -2,7 +2,29 @@
 	<UDashboardPanel v-bind="panelProps">
 		<template #header="headerData">
 			<slot name="header" v-bind="headerData">
-				<UDashboardNavbar v-bind="navbarProps" />
+				<UDashboardNavbar v-bind="navbarProps">
+					<template #leading="leadingData">
+						<slot name="navbar-leading" v-bind="leadingData" />
+					</template>
+					<template #title="titleData">
+						<slot name="navbar-title" v-bind="titleData" />
+					</template>
+					<template #trailing="trailingData">
+						<slot name="navbar-trailing" v-bind="trailingData" />
+					</template>
+					<template #toggle="toggleData">
+						<slot name="navbar-toggle" v-bind="toggleData" />
+					</template>
+					<template #right="rightData">
+						<slot name="navbar-right" v-bind="rightData" />
+					</template>
+					<template #left="leftData">
+						<slot name="navbar-left" v-bind="leftData" />
+					</template>
+					<template #default="defaultData">
+						<slot name="navbar" v-bind="defaultData" />
+					</template>
+				</UDashboardNavbar>
 			</slot>
 		</template>
 		<template #body="bodyData">
