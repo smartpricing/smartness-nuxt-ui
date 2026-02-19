eab5db5b263e2243055c4a3247546d52e0144dbc
Andrea Minato <andreaminato@outlook.com>
2026-02-19 15:50:05 +0100

    feat(sortable-header): add getSortableHeader utility for sortable table headers

--- runtime/index.ts
+++ runtime/index.ts
@@ -1,3 +1,4 @@
 export { useComponentRenderToHTML } from "../app/composables/useComponentRenderToHTML";
+export { getSortableHeader } from "../app/utils/getSortableHeader";
 export type * from "@vueuse/core";
 export { z } from "zod";
