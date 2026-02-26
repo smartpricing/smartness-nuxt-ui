9dbe0a8aa0a6b888dec4a5ff0b749631943e08d2
Andrea Minato <andreaminato@outlook.com>
2026-02-26 11:40:11 +0100

    feat(types): extend ButtonProps and InputProps interfaces to support custom data attributes

--- smartness-ui.d.ts
+++ smartness-ui.d.ts
@@ -7,4 +7,13 @@ declare module "@nuxt/ui" {
 	}
 }
 
+declare module "#ui/types" {
+	interface ButtonProps {
+		[key: `data-${string}`]: unknown
+	}
+	interface InputProps {
+		[key: `data-${string}`]: unknown
+	}
+}
+
 export {};
