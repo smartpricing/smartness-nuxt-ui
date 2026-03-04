43dff9b67adbfccc12af05de93c55a24f50ea48f
Andrea Minato <andreaminato@outlook.com>
2026-03-04 11:24:52 +0100

    feat: enhance Vite configuration with library build options and TypeScript declaration support

--- .smartness-vue/vite.config.ts
+++ .smartness-vue/vite.config.ts
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts';
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+		vue(),
+    dts({
+      cleanVueFileName: true,
+      insertTypesEntry: true
+    })
+	],
+	build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'smartness-vue',
+      formats: ['es', 'umd'],
+      fileName: (format) => `smartness-vue.${format}.js`,
+    },
+    rolldownOptions: {
+      external: ['vue'], // pnpm ensures this is strictly enforced
+      output: {
+        globals: { vue: 'Vue' }
+      }
+    }
+  }
 })
