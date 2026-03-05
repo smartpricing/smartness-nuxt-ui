6e6d135efe4b1af19686657c220edc7a0bc24aea
Andrea Minato <andreaminato@outlook.com>
2026-03-05 12:05:13 +0100

    chore: update pnpm lock files and package dependencies, remove unplugin-auto-import, and integrate Nuxt UI in Vite configuration

--- .smartness-vue/vite.config.ts
+++ .smartness-vue/vite.config.ts
@@ -2,20 +2,35 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 import dts from "vite-plugin-dts";
-import AutoImport from "unplugin-auto-import/vite";
+import nuxtUI from "@nuxt/ui/vite";
 import { resolve } from "path";
+import appConfig from "./src/app.config.ts";
 
 export default defineConfig({
 	plugins: [
-		AutoImport({
-			imports: ["vue"],
-			dts: "src/auto-imports.d.ts",
-		}),
 		vue(),
+		nuxtUI({
+			ui: {
+				...appConfig.ui,
+			},
+			colorMode: false,
+			autoImport: {
+				imports: ["vue"],
+				dts: "src/auto-imports.d.ts",
+			},
+			components:{
+				dirs: [resolve(__dirname, "src/components")],
+				extensions: ["vue"],
+				include: [resolve(__dirname, "src/components/**/*.vue")],
+				exclude: [resolve(__dirname, "src/components/**/*.test.vue")],
+				dts: "src/components.d.ts",
+			}
+		}),
 		tailwindcss(),
 		dts({
 			cleanVueFileName: true,
 			insertTypesEntry: true,
+			tsconfigPath: "./tsconfig.app.json",
 		}),
 	],
 	build: {
